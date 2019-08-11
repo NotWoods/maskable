7f1dcf34825a3c349edadb082270a271baf72232
Tiger Oakes <contact@tigeroakes.com>
2019-08-11 11:37:53 -0400

    Switch to using clip-path

--- src/change-mask.js
+++ src/change-mask.js
@@ -1,35 +1,15 @@
 const defaultMasks = {
-    circle: {
-        borderTopLeftRadius: '50%',
-        borderTopRightRadius: '50%',
-        borderBottomLeftRadius: '50%',
-        borderBottomRightRadius: '50%',
-    },
-    rounded_rect: {
-        borderTopLeftRadius: '34px',
-        borderTopRightRadius: '34px',
-        borderBottomLeftRadius: '34px',
-        borderBottomRightRadius: '34px',
-    },
-    sharp_rect: {
-        borderTopLeftRadius: '0',
-        borderTopRightRadius: '0',
-        borderBottomLeftRadius: '0',
-        borderBottomRightRadius: '0',
-    },
-    drop: {
-        borderTopLeftRadius: '50%',
-        borderTopRightRadius: '50%',
-        borderBottomLeftRadius: '50%',
-        borderBottomRightRadius: '34px',
-    },
+    circle: 'inset(3.5% 3.5% 3.5% 3.5% round 50%)',
+    rounded_rect: 'inset(3.5% 3.5% 3.5% 3.5% round 34px)',
+    sharp_rect: 'inset(3.5% 3.5% 3.5% 3.5%)',
+    drop: 'inset(3.5% 3.5% 3.5% 3.5% round 50% 50% 34px)',
 };
 
 const mask = document.querySelector('.icon__mask');
 document.querySelector('.masks').addEventListener('change', evt => {
     const radio = /** @type {HTMLInputElement} */ (evt.target);
     if (radio.name === 'mask') {
-        Object.assign(mask.style, defaultMasks[radio.value]);
+        mask.style.clipPath = defaultMasks[radio.value];
     }
 });
 
