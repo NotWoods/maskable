c15b4edda85317530f655db69f0e8d659bbe68f1
Tiger Oakes <contact@tigeroakes.com>
2020-07-29 10:09:30 -0700

    Closes #23: Show info for disabled controls (#24)

--- src/editor/options.js
+++ src/editor/options.js
@@ -2,6 +2,10 @@
 
 /** @type {HTMLFormElement} */
 const options = document.querySelector('.options');
+/** @type {HTMLParagraphElement} */
+const backgroundInfo = document.querySelector('.info--background');
+/** @type {HTMLParagraphElement} */
+const fitInfo = document.querySelector('.info--fit');
 
 /**
  * Updates the preview span adjacent to an input with the current value.
@@ -24,7 +28,9 @@ export function selectLayer(layer) {
   options.alpha.value = layer.alpha;
   options.alpha.disabled = layer.locked;
   options.delete.disabled = layer.locked;
+  backgroundInfo.hidden = !layer.locked;
   options.fit[0].disabled = !layer.src;
+  fitInfo.hidden = Boolean(layer.src);
   Array.from(options.elements).forEach(updatePreview);
 }
 
