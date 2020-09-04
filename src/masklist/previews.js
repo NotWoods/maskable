384b32024ee5d8a7a5f7ac0a638c08f015aa4fda
Tiger Oakes <contact@tigeroakes.com>
2020-09-03 21:39:45 -0700

    Add masklist page

---
+++ src/masklist/previews.js
@@ -0,0 +1,23 @@
+import Sortable from '/web_modules/sortablejs.js';
+import { applyMask } from '../viewer/masks.js';
+
+/** @type {NodeListOf<HTMLElement>} */
+const previews = document.querySelectorAll('.mask__preview');
+const lists = document.querySelectorAll('.masklist');
+
+previews.forEach((preview) => {
+  const maskName = preview.parentElement.dataset.value;
+  const success = applyMask([preview], [], maskName);
+
+  if (!success) {
+    preview.parentElement.hidden = true;
+  }
+});
+
+lists.forEach((list) => {
+  new Sortable(list, {
+    group: 'masklist',
+    animation: 150,
+    filter: '.locked',
+  });
+});
--- src/viewer/libs.js
+++ src/viewer/libs.js
@@ -12,7 +12,9 @@ const ad = document.querySelector('[data-ea-publisher]');
  */
 function updateDarkModeClasses(darkMode) {
   document.body.classList.toggle('dark', darkMode);
-  ad.classList.toggle('dark', darkMode)
+  if (ad) {
+    ad.classList.toggle('dark', darkMode);
+  }
 }
 
 // Initialize the toggle based on `prefers-color-scheme`, defaulting to 'light'.
@@ -20,14 +22,14 @@ toggle.mode = matchMedia('(prefers-color-scheme: dark)').matches
   ? 'dark'
   : 'light';
 // Set or remove the `dark` class the first time.
-updateDarkModeClasses(toggle.mode === 'dark')
+updateDarkModeClasses(toggle.mode === 'dark');
 // Listen for toggle changes (which includes `prefers-color-scheme` changes)
 // and toggle the `dark` class accordingly.
 toggle.addEventListener('colorschemechange', () => {
   updateDarkModeClasses(toggle.mode === 'dark');
 });
 
-if (document.monetization) {
+if (document.monetization && ad) {
   document.monetization.addEventListener('monetizationstart', () => {
     if (document.monetization.state === 'started') {
       console.log('Payment started, hiding ads');
