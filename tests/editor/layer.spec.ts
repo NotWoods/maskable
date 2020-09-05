8486f85120d61b9d6038bae781dca930eb707146
Tiger Oakes <contact@tigeroakes.com>
2020-09-05 14:29:30 -0700

    Add editor test

---
+++ tests/editor/layer.spec.ts
@@ -0,0 +1,46 @@
+// @ts-check
+
+import { backgroundLayer, createLayer } from '../../src/editor/layer.js';
+
+describe('createLayer', () => {
+  test('create layer with fill', () => {
+    expect(createLayer('#f00')).toEqual({
+      name: 'Layer',
+      fill: '#f00',
+      padding: 0,
+      x: 0,
+      y: 0,
+      alpha: 100,
+      locked: false,
+      fit: 'contain',
+    });
+  });
+
+  test('create layer with image', () => {
+    const img = { close() {} } as ImageBitmap;
+    expect(createLayer('#ff0', img)).toEqual({
+      src: img,
+      name: 'Layer',
+      fill: '#ff0',
+      padding: 0,
+      x: 0,
+      y: 0,
+      alpha: 0,
+      locked: false,
+      fit: 'contain',
+    });
+  });
+
+  test('create background layer', () => {
+    expect(backgroundLayer()).toEqual({
+      name: 'Layer',
+      fill: '#448AFF',
+      padding: 0,
+      x: 0,
+      y: 0,
+      alpha: 100,
+      locked: true,
+      fit: 'contain',
+    });
+  });
+});
