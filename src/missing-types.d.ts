e27bf42af4c16252c1cffeec42b3c1092ddc5055
Tiger Oakes <contact@tigeroakes.com>
2023-03-19 10:50:18 -0700

    Update deps

--- src/missing-types.d.ts
+++ src/missing-types.d.ts
@@ -8,10 +8,16 @@ interface Fathom {
 }
 
 interface ColorSelectionOptions {
+  /**
+   * An `AbortSignal`. The eyedropper mode will be aborted when the `AbortSignal`'s `abort()` method is called.
+   */
   signal?: AbortSignal;
 }
 
 interface ColorSelectionResult {
+  /**
+   * A string representing the selected color, in hexadecimal sRGB format (`#aabbcc`).
+   */
   sRGBHex: string;
 }
 
@@ -24,13 +30,23 @@ declare global {
     filedrop: FileDropEvent;
   }
 
+  /**
+   * The `EyeDropper` interface represents an instance of an eyedropper tool that can be opened and used by the user to select colors from the screen.
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper
+   */
   class EyeDropper {
     constructor();
+    /**
+     * The `EyeDropper.open()` method starts the eyedropper mode,
+     * returning a promise which is fulfilled once the user has either selected a color or dismissed the eyedropper mode.
+     * @param options An options object to pass an `AbortSignal` signal.
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper/open
+     */
     open(options?: ColorSelectionOptions): Promise<ColorSelectionResult>;
   }
 
   interface Window {
-    EyeDropper: typeof EyeDropper;
+    EyeDropper?: typeof EyeDropper;
   }
 
   let fathom: Fathom;
