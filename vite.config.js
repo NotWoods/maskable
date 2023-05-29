8f26843252f88f20666a6e018f639b53d97a5328
Tiger Oakes <contact@tigeroakes.com>
2023-05-29 09:56:15 -0700

    Use type: module

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { resolve } from 'path';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 import handlebars from 'vite-plugin-handlebars';
 import { VitePWA as pwa } from 'vite-plugin-pwa';
 import { ViteWebfontDownload as webfont } from 'vite-plugin-webfont-dl';
 
-module.exports = defineConfig({
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default defineConfig({
   build: {
     minify: false,
     rollupOptions: {
